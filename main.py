92bbedc6ae3ea9ebe217eda23b7008b27fe9c5e5
Alongbar Basumatary <155052855+alongbarbasumatary@users.noreply.github.com>
2026-04-17 11:46:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import os
-from flask import Flask
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
-PORT = int(os.environ.get("PORT", 10000))
 
 if not TOKEN:
     print("BOT_TOKEN missing")
     exit(1)
 
-# ---------- Telegram Bot ----------
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     u = update.effective_user
 
@@ -26,23 +23,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text(text)
 
-# ---------- Web Server ----------
-web = Flask(__name__)
-
-@web.route("/")
-def home():
-    return "Bot is running 🚀"
-
 def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
 
-    # start bot WITHOUT blocking Flask
-    app.run_polling(close_loop=False)
+    print("🚀 Bot running...")
+    app.run_polling()
 
 if __name__ == "__main__":
-    import threading
-
-    threading.Thread(target=main).start()
-
-    web.run(host="0.0.0.0", port=PORT)
+    main()
